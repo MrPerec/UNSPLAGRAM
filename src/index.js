85cadfb7c066fc9ae99965612e9cb8932c143df5
MrPerec <MrPerec@mail.ru>
2021-09-06 15:26:07 +0400

    commented scroll logic

--- src/index.js
+++ src/index.js
@@ -9,31 +9,24 @@ import './styles/style.css';
 import App from './containers/App.js';
 import rootReducer from './reducers/reducer';
 import {addPostPhoto} from './actions/actions';
-import {SCROLL_HEIGHT} from './constants/constants.js';
+// import {SCROLL_HEIGHT} from './constants/constants.js';
 
-const $htmlElem = document.querySelector(`.js_html`);
 const $rootElem = document.querySelector(`.js-root`);
 
-const addPostPhotoEndOfScroll = () =>{
+/* const addPostPhotoEndOfScroll = () =>{
     let scrollHeight = Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight, 
         document.body.offsetHeight,	document.documentElement.offsetHeight, 
         document.body.clientHeight, document.documentElement.clientHeight
     );
-    // console.log(scrollHeight)
-    // console.log(innerHeight)
-    // console.log(scrollHeight - innerHeight)
-    // console.log(scrollY)
-    // console.log(scrollY - SCROLL_HEIGHT)
     if (scrollHeight - innerHeight === scrollY || scrollHeight - innerHeight === scrollY - SCROLL_HEIGHT) return store.dispatch( addPostPhoto() );
 };
 
-// window.addEventListener('scroll', addPostPhotoEndOfScroll);
+window.addEventListener('scroll', addPostPhotoEndOfScroll); */
 
 const store = createStore( rootReducer, applyMiddleware(thunk) );
 store.dispatch( addPostPhoto() );
 
 render( 
-    <App store={store}/>, $rootElem,
-    // onScroll={addPostPhoto}, $htmlElem 
+    <App store={store}/>, $rootElem, 
 );

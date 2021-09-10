96ea557a2ba82b554c93cfcb4b96681bf0783c09
MrPerec <MrPerec@mail.ru>
2021-09-10 20:03:04 +0400

    add scroll logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { render }  from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {addPostPhoto} from './actions/actions';
 
 import './styles/style.css';
 import App from './containers/App.js';
@@ -13,6 +14,15 @@ import reducers from './reducers/rootReducer';
 const store = createStore(reducers, composeWithDevTools(
     applyMiddleware(thunk),
 ));
+
+window.addEventListener('scroll', () =>{
+    let scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight, 
+        document.body.offsetHeight,	document.documentElement.offsetHeight, 
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+    if (scrollHeight - innerHeight === scrollY) return store.dispatch( addPostPhoto() );
+});
 
 render( 
     <App store={store}/>, document.querySelector(`.js-root`)

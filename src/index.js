27fa5aa79cd4f7b042ff1dc1a389059473e71bc2
MrPerec <MrPerec@mail.ru>
2021-09-10 16:27:06 +0400

    del scroll logic, add devTools extention

--- src/index.js
+++ src/index.js
@@ -2,27 +2,17 @@
 
 import React from 'react';
 import { render }  from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './styles/style.css';
 import App from './containers/App.js';
 import reducers from './reducers/rootReducer';
-import { addPostPhoto } from './actions/actions';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(
-    applyMiddleware(thunk)
+const store = createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunk),
 ));
-
-window.addEventListener('scroll', () =>{
-    let scrollHeight = Math.max(
-        document.body.scrollHeight, document.documentElement.scrollHeight, 
-        document.body.offsetHeight,	document.documentElement.offsetHeight, 
-        document.body.clientHeight, document.documentElement.clientHeight
-    );
-    if (scrollHeight - innerHeight === scrollY) return store.dispatch( addPostPhoto() );
-});
 
 render( 
     <App store={store}/>, document.querySelector(`.js-root`)

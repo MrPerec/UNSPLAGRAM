178bea56d8879df7395b0bbfcdb0ba8bb2679114
MrPerec <MrPerec@mail.ru>
2021-09-18 18:03:46 +0400

    edit navlink to link

--- src/components/DisplayPostPhoto.js
+++ src/components/DisplayPostPhoto.js
@@ -2,39 +2,55 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import '../styles/post.css';
 import '../styles/fonts/iconfont/flaticon.css';
 
-export default function DisplayPostPhoto({postOfPhoto}){
-	const { urlsSmall, altDescription, userName, userLinksHtml, createdAt, likes} = postOfPhoto;
-	
-	return (
-		<div className='post_container'>
-			<NavLink to={'/photoFullSize'}>
-				<img 
-					className='image'
-					src={urlsSmall}
-					alt={altDescription}
-					title={altDescription}
-				/>
-			</NavLink>
-			<div className='post_text_container'>
-				<div className='post_name_piblish_container'>
-					<a href={userLinksHtml} target='_blank'	rel='noopener noreferrer' className="user_link">{userName}</a>
-					<time dateTime={createdAt} className="publish_style">Published on {createdAt} </time>
-				</div>
-				<div className='like_container'>
-					<i className='flaticon-heart flaticon_style flaticon-heart_style'></i>
-					{/* <i className='flaticon-like flaticon_style flaticon-like_style'></i> */}
-					{likes}
-				</div> 
-			</div>
-		</div>
-	);
+export default function DisplayPostPhoto({ postOfPhoto }) {
+  const {
+    urlsSmall,
+    altDescription,
+    userName,
+    userLinksHtml,
+    createdAt,
+    likes,
+  } = postOfPhoto;
+
+  return (
+    <div className='post_container'>
+      <Link to={'/photoFullSize'}>
+        <img
+          className='image'
+          src={urlsSmall}
+          alt={altDescription}
+          title={altDescription}
+        />
+      </Link>
+      <div className='post_text_container'>
+        <div className='post_name_piblish_container'>
+          <a
+            href={userLinksHtml}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='user_link'
+          >
+            {userName}
+          </a>
+          <time dateTime={createdAt} className='publish_style'>
+            Published on {createdAt}{' '}
+          </time>
+        </div>
+        <div className='like_container'>
+          <i className='flaticon-heart flaticon_style flaticon-heart_style'></i>
+          {/* <i className='flaticon-like flaticon_style flaticon-like_style'></i> */}
+          {likes}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 DisplayPostPhoto.propTypes = {
-	postOfPhoto: PropTypes.object.isRequired
-}
+  postOfPhoto: PropTypes.object.isRequired,
+};

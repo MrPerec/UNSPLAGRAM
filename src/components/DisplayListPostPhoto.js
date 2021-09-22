8838a5cc4dfc87036b758fc367ea5246d0909f6b
MrPerec <MrPerec@mail.ru>
2021-09-22 18:07:46 +0400

    add infinitiScroll logic

--- src/components/DisplayListPostPhoto.js
+++ src/components/DisplayListPostPhoto.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import DisplayPostPhoto from './DisplayPostPhoto.js';
 import '../styles/postList.css';
 import { INITIAL_STATE_LENGTH } from '../constants/constants.js';
@@ -36,7 +37,17 @@ export default function DisplayListPostPhoto({ postPhoto, addPostPhoto }) {
         likes,
       };
 
-      return <DisplayPostPhoto key={id} postOfPhoto={postOfPhoto} />;
+      // return <DisplayPostPhoto key={id} postOfPhoto={postOfPhoto} />;
+      return (
+        <InfiniteScroll
+          dataLength={postPhoto.length}
+          next={addPostPhoto}
+          hasMore={true}
+          // loader={<h4>Loading...</h4>}
+        >
+          <DisplayPostPhoto key={id} postOfPhoto={postOfPhoto} />
+        </InfiniteScroll>
+      );
     }
   });
 
@@ -44,6 +55,7 @@ export default function DisplayListPostPhoto({ postPhoto, addPostPhoto }) {
 
   return (
     <div className='post_list_container'>{listOfPhotosPosts}</div>
+
     // <div>
     //   <div className='post_list_container'>{listOfPhotosPosts}</div>
     //   <h4 className='post_list_container'>Loading...</h4>

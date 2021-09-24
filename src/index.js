c8a52b20a0921fd4fadb415db424f1dee07f46f0
MrPerec <MrPerec@mail.ru>
2021-09-24 14:53:27 +0400

    del scroll logic in comments

--- src/index.js
+++ src/index.js
@@ -9,47 +9,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './containers/App.js';
 import reducers from './reducers/rootReducer';
-// import { addPostPhoto } from './actions/actions';
 
 const store = createStore(
   reducers,
   composeWithDevTools(applyMiddleware(thunk))
 );
-
-/* const onScroll = () => {
-  const height = Math.max(
-    document.body.scrollHeight,
-    document.documentElement.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.offsetHeight,
-    document.body.clientHeight,
-    document.documentElement.clientHeight
-  );
-  const screenHeight = window.innerHeight;
-  const scrolled = window.scrollY;
-  const threshold = height - screenHeight;
-  const position = scrolled + screenHeight;
-
-  if (threshold <= position) return store.dispatch(addPostPhoto());
-};
-
-function throttle(callee, timeout) {
-  let timer = null;
-
-  return function perform(...args) {
-    if (timer) return;
-
-    timer = setTimeout(() => {
-      callee(...args);
-
-      clearTimeout(timer);
-      timer = null;
-    }, timeout);
-  };
-}
-
-window.addEventListener(`scroll`, throttle(onScroll, 1000));
-window.addEventListener(`resize`, throttle(onScroll, 1000)); */
 
 render(
   <Router>

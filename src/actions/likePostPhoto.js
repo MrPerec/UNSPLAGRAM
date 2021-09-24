bd5f0c4bd954bf6d766f5709718a1664c5df12ac
MrPerec <MrPerec@mail.ru>
2021-09-24 08:24:58 +0400

    add likePost

--- src/actions/likePostPhoto.js
+++ src/actions/likePostPhoto.js
@@ -1,1 +1,9 @@
-`use sctrict`;
+`use strict`;
+
+import { LIKE_POST_PHOTO } from '../types/types.js';
+
+export function likePostPhoto() {
+  dispatch({
+    type: LIKE_POST_PHOTO,
+  });
+}
--- src/types/types.js
+++ src/types/types.js
@@ -1,3 +1,4 @@
 `use strict`;
 
 export const ADD_POST_PHOTO = `ADD_POST_PHOTO`;
+export const LIKE_POST_PHOTO = `LIKE_POST_PHOTO`;

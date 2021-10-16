3825026273d75510ec4ff8f43ef83098d7da6cda
MrPerec <MrPerec@mail.ru>
2021-10-16 19:04:17 +0400

    add comments with route and url

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,9 +3,22 @@
 import { createApi } from 'unsplash-js';
 import { ACCESS_KEY, SECRET_KEY } from '../constants/constants.js';
 
+const LIKE_URL = `https://api.unsplash.com/photos/?client_id=_JCTLXIriKQH3zfw4IPJa7c4uUf-KBTJbCqNHGAvXFc&id=6uneKLGrJPs/like`;
+
 const unsplash = createApi({
   accessKey: ACCESS_KEY,
   secret: SECRET_KEY,
 });
 
 export default unsplash;
+
+// router.push({
+//   pathname: 'https://unsplash.com/oauth/authorize',
+//   query: {
+//     redirect_uri: 'http://localhost:9000/',
+//     client_id: 'MY_CLIENT_ID',
+//     response_type: 'code',
+//     scope:
+//       'public+read_user+read_photos+write_photos+write_likes+write_followers+read_collections+write_collections',
+//   },
+// });

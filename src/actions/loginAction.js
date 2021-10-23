418cc2d41bdbdb10c6ab857dd00e213d4aa77960
MrPerec <MrPerec@mail.ru>
2021-10-23 21:29:08 +0400

    add comments

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -1,11 +1,17 @@
 `use strict`;
 
+import { createApi } from 'unsplash-js';
 import { LOGIN } from '../constants/types.js';
 import { ACCESS_KEY, REDIRECT_URI } from '../constants/constants.js';
 
 // const SOME_URL = `https://unsplash.com/photos/?client_id=_JCTLXIriKQH3zfw4IPJa7c4uUf-KBTJbCqNHGAvXFc`
-// const q = `https://unsplash.com/oauth/authorize/?client_id=_JCTLXIriKQH3zfw4IPJa7c4uUf-KBTJbCqNHGAvXFc`;
-// const AUTH_URL = `https://unsplash.com/oauth/authorize`;
+
+// url для авторизации
+// const AUTH_URL = `https://unsplash.com/oauth/authorize/?client_id=_JCTLXIriKQH3zfw4IPJa7c4uUf-KBTJbCqNHGAvXFc&redirect_uri=http://localhost:3000/login&response_type=code&scope=public+write_likes`;
+
+//тут, после авторизации на unsplash, сохраняется code при помощи которого
+//можно запросить и получить токен доступа
+// window.location.search
 
 // const configObj = {
 //   client_id: ACCESS_KEY,
@@ -21,6 +27,13 @@ const unsplash = createApi({
   scope: `public+write_likes`,
   secret: SECRET_KEY,
 });
+
+const authenticationUrl = unsplash.auth.getAuthenticationUrl([
+  'public',
+  'write_likes',
+]);
+
+location.assign(authenticationUrl);
 
 // const promise = fetch(AUTH_URL, configObj).then((response) => {
 //   console.log(response);

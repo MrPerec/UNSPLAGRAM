80338bdeec2073ea3b4b5cdd14b625ffd614011c
MrPerec <MrPerec@mail.ru>
2021-11-17 07:18:00 +0400

    rename item to photo

--- src/actions/addPhotoAction.js
+++ src/actions/addPhotoAction.js
@@ -6,18 +6,18 @@ import { ADD_PHOTO } from '../constants/types.js';
 export default function addPhotoAction() {
   return (dispatch) => {
     requestPhotoList().then((response) => {
-      response.forEach((item) => {
+      response.forEach((photo) => {
         dispatch({
           type: ADD_PHOTO,
-          id: item.id,
-          urlsRegular: item.urls.regular,
-          urlsSmall: item.urls.small,
-          altDescription: item.alt_description,
-          userName: item.user.name,
-          userLinksHtml: item.user.links.html,
-          createdAt: item.created_at,
-          likes: item.likes,
-          likedByUser: item.liked_by_user,
+          id: photo.id,
+          urlsRegular: photo.urls.regular,
+          urlsSmall: photo.urls.small,
+          altDescription: photo.alt_description,
+          userName: photo.user.name,
+          userLinksHtml: photo.user.links.html,
+          createdAt: photo.created_at,
+          likes: photo.likes,
+          likedByUser: photo.liked_by_user,
         });
       });
     });

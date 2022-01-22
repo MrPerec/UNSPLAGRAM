30d4f8b2df596445afe61941cadf6c9241cdcd7e
MrPerec <MrPerec@mail.ru>
2022-01-22 13:14:38 +0400

    refactoring header, css

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -2,50 +2,50 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import { AUTH_URL } from '../../constants/constants.js';
+
 import '../../styles/header.css';
 import '../../styles/auth.css';
-import { AUTH_URL } from '../../constants/constants.js';
 
 export default function Header({ auth, logoutAction }) {
   const { login, userName, profileImage } = auth;
+  const history = useHistory();
   const onLogout = () => {
     logoutAction();
+    history.push(`/`);
     window.location.reload();
   };
 
-  const avatarAuthState = userName ? (
-    <div>
+  const authState = login ? (
+    <div className='auth_container auth_container_style'>
       <img
         className='avatar avatar_style auth_text_style'
         src={profileImage}
         alt={userName}
       />
       <div className='user-name auth_text_style'>{userName}</div>
+      <button
+        className='button-sign-in button-sign-in__style auth_text_style'
+        onClick={onLogout}
+      >
+        Log Out
+      </button>
     </div>
-  ) : null;
-
-  const buttonAuthState = !login ? (
-    <a
-      href={AUTH_URL}
-      className='button-sign-in button-sign-in__style auth_text_style'
-    >
-      Log In
-    </a>
   ) : (
-    <button
-      className='button-sign-in button-sign-in__style auth_text_style'
-      onClick={onLogout}
-    >
-      Log Out
-    </button>
+    <div className='auth_container auth_container_style'>
+      <a
+        href={AUTH_URL}
+        className='button-sign-in button-sign-in__style auth_text_style'
+      >
+        Log In
+      </a>
+    </div>
   );
 
   return (
     <header className='header header__container gray__block'>
-      <div className='auth_container auth_container_style'>
-        {avatarAuthState}
-        {buttonAuthState}
-      </div>
+      {authState}
       <h3 className='h3 header_h3__text-style'>UNSPLAGRAM</h3>
     </header>
   );

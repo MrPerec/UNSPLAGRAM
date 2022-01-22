1fac4844f4212d4b6789184ae40ff6b6cc2dbe87
MrPerec <MrPerec@mail.ru>
2022-01-22 13:13:15 +0400

    add redirect authomaticly. add css

--- src/components/layouts/AuthPage.js
+++ src/components/layouts/AuthPage.js
@@ -2,22 +2,28 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import ButtonGoHome from './ButtonGoHome';
-import '../../styles/button.css';
+import ButtonGoMain from './ButtonGoMain';
+import { TIMER } from '../../constants/constants.js';
+import '../../styles/authPage.css';
 
 export default function AuthPage({ auth, loginAction }) {
   const { login, userName } = auth;
-
   const authStatus = !login
     ? `You are not authorized`
     : `Greating you ${userName}!`;
 
   if (!login) loginAction();
 
+  setTimeout(() => {
+    window.location.href = '/';
+  }, TIMER);
+
   return (
-    <div>
-      <h2 className='button_container'>{authStatus}</h2>
-      <ButtonGoHome />
+    <div className='auth-page_style'>
+      <h2>{authStatus}</h2>
+      <div>You will be redirect to the main page in 5 second.</div>
+      <div>If it doesn't happen please click the button below.</div>
+      <ButtonGoMain />
     </div>
   );
 }

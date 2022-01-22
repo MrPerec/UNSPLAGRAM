bdfd00eb2eaae513865889287347a9463bb4c17d
MrPerec <MrPerec@mail.ru>
2022-01-22 13:14:13 +0400

    smooth scroll

--- src/components/layouts/ButtonUp.js
+++ src/components/layouts/ButtonUp.js
@@ -1,12 +1,20 @@
 `use strict`;
 
 import React from 'react';
+import { ZERO, VERTICAL_OFFSET } from '../../constants/constants.js';
 import '../../styles/button.css';
 import '../../styles/buttonUp.css';
 
 export default function ButtonUp() {
+  const onUp = () => {
+    if (window.pageYOffset > ZERO) {
+      window.scrollBy(ZERO, VERTICAL_OFFSET);
+      setTimeout(onUp, ZERO);
+    }
+  };
+
   return (
-    <a className='button-up button-up__style' href='#'>
+    <a className='button-up button-up__style' onClick={onUp}>
       <i className='flaticon-up-arrow'></i>
     </a>
   );
